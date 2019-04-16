2d198012680aea1bc16fb1d7583980bfb4bd5e8d
jaimeDespegar <jaime.quispe@despegar.com>
2019-04-16 18:58:57 -0300

    actualizo gitignore

--- ungsmaps/src/main/java/main/App.java
+++ ungsmaps/src/main/java/main/App.java
@@ -10,7 +10,7 @@ public class App {
     public static void main(String[] args) {
 
         LocationProxy service = new ServiceProxy(new MockLocationFactory());
-        service.getRoad(new LocationSearchBuilder().withArrival(23,41).withDeparture(50, 60).build())
+        service.getRoad(new LocationSearchBuilder().withArrival(22,66).withDeparture(22, 66).build())
                .forEach(i->System.out.println(i.getX()+ " - "+ i.getY()));
 
     }
