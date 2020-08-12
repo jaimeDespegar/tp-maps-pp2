73e6d6640879fcbf6ac74faa3c119473337fd509
jaime.quispe <jaime.quispe@despegar.com>
2020-08-12 11:03:16 -0300

    wip

--- ungsmaps/src/main/java/core/CustomerClassLoader.java
+++ ungsmaps/src/main/java/core/CustomerClassLoader.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 public class CustomerClassLoader {
 
-    public LocationConnector loaded(String pathName, String className) {
+    public <C> C loaded(String pathName, String className) {
         File file = new File(pathName);
         File[] files = Optional.of(file).map(File::listFiles).orElse(new File[]{});
         List<File> jars = Arrays.asList(files).stream().filter(i->i.getPath().endsWith(".jar")).collect(Collectors.toList());
@@ -26,7 +26,7 @@ public class CustomerClassLoader {
         }
         URLClassLoader childClassLoader = new URLClassLoader(urls, ClassLoader.getSystemClassLoader());
         try {
-            return (LocationConnector) Class.forName(className, true, childClassLoader).newInstance();
+            return (C) Class.forName(className, true, childClassLoader).newInstance();
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException c) {
             throw new RuntimeException("Error cargando la clase");
         }
@@ -34,7 +34,7 @@ public class CustomerClassLoader {
 
     public static void main(String[] args) {
         CustomerClassLoader loader = new CustomerClassLoader(); // ApiLocationMock  LocationCalculatorConnector
-        LocationConnector connector = loader.loaded("/home/jaimequispe/ungs/pp2/tp-maps-pp2/ungsmaps/jars", "implementation.LocationCalculatorConnector");
+        LocationConnector connector = loader.loaded("/home/jaimequispe/ungs/pp2/tp-maps-pp2/ungsmaps/jars", "implementation.ApiLocationMock");
         System.out.println("connector is ok : " + connector.toString());
     }
 
